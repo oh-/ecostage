860fa5423776829608d8c0f2e25907e6525d32b9
Samuel Overington <so@madeso.uk>
2015-04-23 19:24:52 +0100

    compass is working

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,52 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var sass = require('gulp-sass');
+var compass = require('gulp-compass');
+var plumber = require('gulp-plumber')
 var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
 
-var SassFiles = {
-	soc: 'app/theme/sass/style.scss',
-	dst: 'app/theme/',
-	sf: './app/theme/sass/{,*/}*.{scss,sass}'
-};
+var paths = {
+       	SassFiles : {
+		soc: 'app/theme/sass/style.scss',
+		dst: 'app/theme/',
+		sf: './app/theme/sass/{,*/}*.{scss,sass}'
+	},
+	styles: {
+		src: './app/theme/sass/{,*/}*.{scss,sass}',
+		sass: './app/theme/sass/',
+		dest: './app/theme/',
+		bower: './bower_components/',
+		build: './app/temp/'
+		}
+}
+
+ 
+gulp.task('compass', function() {
+  gulp.src(paths.styles.src)
+	.pipe(plumber({
+		errorHandler: function (error) {
+			console.log(error.message);
+			this.emit('end');
+		}
+	}))
+    .pipe(compass({
+		import_path: paths.styles.bower,
+		config_file: './config.rb',
+		require: 'susy',
+		css: paths.styles.dest ,
+		sass: paths.styles.sass
+    }))
+	.on('error', function(err) {
+		//would like to catch the error here
+	})
+    .pipe(gulp.dest(paths.styles.build))
+    .pipe(reload({ stream:true }));
+});
+
+
 
 // Gulp Sass Task 
 gulp.task('sass', function() {
@@ -40,7 +76,7 @@ gulp.task('serve', function() {
 	gulp.watch('app/theme/**.scss', ['sass']);
 });
 
-gulp.task('default', ['sass', 'serve'], function() {
-	// gulp.watch('./app/theme/sass/**.scss', ['sass']);
+gulp.task('default', ['compass', 'serve'], function() {
+	gulp.watch('./app/theme/sass/**.scss', ['compass']);
 	gulp.watch('./app/theme/sass/{,*/}*.{scss,sass}', ['sass']);
 });

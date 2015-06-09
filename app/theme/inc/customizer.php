d31b7c383f70f90ef74b6bfcce6156e0c35db5a9
Samuel Overington (neon) <so@madeso.uk>
2015-06-10 00:40:13 +0100

    on the way The options page is working section added - need to title it setting and controller is added need to figure out how to print it in the theme

--- app/theme/inc/customizer.php
+++ app/theme/inc/customizer.php
@@ -14,23 +14,179 @@ function _s_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
-	$wp_customize->get_section( 'header-infi' )->transport = 'postMessage';
-	$wp_customize->add_section( 'header_infi', array(
-		'title' => __('Main Header info'),
-		'description' => __('Add the Header paragraph and button text here'),
+	$wp_customize->add_section( 'new_section', array(
+		'title'	=> __('Testing new section','_s'),
+		'priority' => 30,
 	) );
-	$wp_customize->add_setting( '_s_options[call]', array(
-		'type' => 'theme_mod', //  or 'option'
-		'defaults' => 'We are fighting a cause!',
-		'sanitize_callback' => 'call_to_action',
+
+	// Add color scheme setting and control.
+	$wp_customize->add_setting( 'color_scheme', array(
+		'default'           => 'default',
+		'sanitize_callback' => '_s_color_scheme',
+		'transport'         => 'postMessage',
 	) );
-	$wp_customize->add_control('call_to_action', array(
-		'label' => __('Call To Action Text'),
-		'type' => 'textarea', // core provides checkbox, textarea, radio( array('key' => 'value')), select, array('key' => 'value') dropdown-pages. or any HTML <input> element
-		'section' => 'header_infi',
+
+	$wp_customize->add_control( 'color_scheme', array(
+		'label'    => __( 'Base Color Scheme', '_s' ),
+		'section'  => 'colors',
+		'type'     => 'select',
+		'choices'  => _s_get_color_scheme_choices(),
+		'priority' => 1,
 	) );
+	
+	// new settings
+	$wp_customize->add_setting( 'new_setting', array(
+		'default'           => 'default',
+		'sanitize_callback' => '_s_new_setting',
+		'transport'         => 'postMessage',
+	) );
+
+	$wp_customize->add_control( 'new_setting', array(
+		'label'    => __( 'NEw Setting', '_s' ),
+		'section'  => 'new_section',
+		'type'     => 'textarea',
+		'priority' => 60,
+	) );
+
+/** 
+'type' properties for control settings
+    text (default)
+    checkbox
+    radio (requires choices array in $args)
+    select (requires choices array in $args)
+    dropdown-pages
+    textarea (since WordPress 4.0)
+ */
+	// Add custom header and sidebar background color setting and control.
+	$wp_customize->add_setting( 'header_background_color', array(
+		'default'           => $color_scheme[1],
+		'sanitize_callback' => 'sanitize_hex_color',
+		'transport'         => 'postMessage',
+	) );
+
+	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'header_background_color', array(
+		'label'       => __( 'Header and Sidebar Background Color', '_s' ),
+		'description' => __( 'Applied to the header on small screens and the sidebar on wide screens.', '_s' ),
+		'section'     => 'colors',
+	) ) );
+
+	// Add an additional description to the header image section.
+	$wp_customize->get_section( 'header_image' )->description = __( 'Applied to the header on small screens and the sidebar on wide screens.', '_s' );
+
 }
 add_action( 'customize_register', '_s_customize_register' );
+
+/**
+ * Register color schemes for Twenty Fifteen.
+ *
+ * Can be filtered with {@see '_s_color_schemes'}.
+ *
+ * The order of colors in a colors array:
+ * 1. Main Background Color.
+ * 2. Sidebar Background Color.
+ * 3. Box Background Color.
+ * 4. Main Text and Link Color.
+ * 5. Sidebar Text and Link Color.
+ * 6. Meta Box Background Color.
+ *
+ * @since Twenty Fifteen 1.0
+ *
+ * @return array An associative array of color scheme options.
+ */
+function _s_get_color_schemes() {
+	return apply_filters( '_s_color_schemes', array(
+		'default' => array(
+			'label'  => __( 'Default', '_s' ),
+			'colors' => array(
+				'#f1f1f1',
+				'#ffffff',
+				'#ffffff',
+				'#333333',
+				'#333333',
+				'#f7f7f7',
+			),
+		),
+		'dark'    => array(
+			'label'  => __( 'Dark', '_s' ),
+			'colors' => array(
+				'#111111',
+				'#202020',
+				'#202020',
+				'#bebebe',
+				'#bebebe',
+				'#1b1b1b',
+			),
+		),
+		'yellow'  => array(
+			'label'  => __( 'Yellow', '_s' ),
+			'colors' => array(
+				'#f4ca16',
+				'#ffdf00',
+				'#ffffff',
+				'#111111',
+				'#111111',
+				'#f1f1f1',
+			),
+		),
+		'pink'    => array(
+			'label'  => __( 'Pink', '_s' ),
+			'colors' => array(
+				'#ffe5d1',
+				'#e53b51',
+				'#ffffff',
+				'#352712',
+				'#ffffff',
+				'#f1f1f1',
+			),
+		),
+		'purple'  => array(
+			'label'  => __( 'Purple', '_s' ),
+			'colors' => array(
+				'#674970',
+				'#2e2256',
+				'#ffffff',
+				'#2e2256',
+				'#ffffff',
+				'#f1f1f1',
+			),
+		),
+		'blue'   => array(
+			'label'  => __( 'Blue', '_s' ),
+			'colors' => array(
+				'#e9f2f9',
+				'#55c3dc',
+				'#ffffff',
+				'#22313f',
+				'#ffffff',
+				'#f1f1f1',
+			),
+		),
+	) );
+}
+/**
+ * Colorscheme choices
+ */
+
+
+if ( ! function_exists( '_s_get_color_scheme_choices' ) ) :
+/**
+ * Returns an array of color scheme choices registered for Twenty Fifteen.
+ *
+ * @since Twenty Fifteen 1.0
+ *
+ * @return array Array of color schemes.
+ */
+function _s_get_color_scheme_choices() {
+	$color_schemes                = _s_get_color_schemes();
+	$color_scheme_control_options = array();
+
+	foreach ( $color_schemes as $color_scheme => $value ) {
+		$color_scheme_control_options[ $color_scheme ] = $value['label'];
+	}
+
+	return $color_scheme_control_options;
+}
+endif; // _s_get_color_scheme_choices
 
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.

666afbc8a8cf8c074fb657de27865ab2aef28d1c
Samuel Overington (neon) <so@madeso.uk>
2015-11-13 17:59:19 +0000

    updated footer

--- footer.php
+++ footer.php
@@ -11,12 +11,15 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+<div>
+<a href="<?php echo esc_url('http://twitter.com', 'eco' ); ?>"><img src="<?php echo get_stylesheet_directory_uri();?>/img/t_button.png" width="30px" height="30px" alt="twitter" /></a>
+<a href="<?php echo esc_url('http://facebook.com', 'eco'); ?>"><img src="<?php echo get_stylesheet_directory_uri();?>/img/f_button.png" width="30px" height="30px" alt="facebook" /></a>
+</div>
 		<div class="site-info">
 			Site: <a href="<?php echo esc_url( __( 'http://madeso.uk/r/eco/', 'eco' ) ); ?>">made<strong>so</strong></a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
-
 <?php wp_footer(); ?>
 
 </body>

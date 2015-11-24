54cc74555dd9ae6de55f84e562e45f43fb48c1b4
Samuel Overington (neon) <so@madeso.uk>
2015-11-24 16:50:18 +0000

    updated twitter

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 <div class='social-buttons'>
 <a href="<?php echo esc_url('https://www.facebook.com/groups/417324151803310/', 'eco'); ?>"><img src="<?php echo get_stylesheet_directory_uri();?>/img/f_button.png" width="45px" height="45px" alt="facebook" /></a>
-<a href="<?php echo esc_url('https://twitter.com/hashtag/ecostage?f=tweets&vertical=default&src=hash', 'eco' ); ?>"><img src="<?php echo get_stylesheet_directory_uri();?>/img/t_button.png" width="45px" height="45px" alt="twitter" /></a>
+<a href="<?php echo esc_url('https://twitter.com/ecostagepledge', 'eco' ); ?>"><img src="<?php echo get_stylesheet_directory_uri();?>/img/t_button.png" width="45px" height="45px" alt="twitter" /></a>
 </div>
 		<div class="site-info">
 			Site: <a href="<?php echo esc_url( __( 'http://madeso.uk/r/eco/', 'eco' ) ); ?>">made<strong>so</strong></a>

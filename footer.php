317a449147af75fe180bd3da1b38fd1edbebd81f
Samuel Overington (neon) <so@madeso.uk>
2015-12-07 19:57:40 +0000

    fixed footer to be only one footer fixed header to be properly aligned started fixing menu - but not completed yet

--- footer.php
+++ footer.php
@@ -13,8 +13,8 @@
 	<footer id="colophon" class="site-footer band" role="contentinfo">
   <div id="footer-var" class='band'>
     <div id="social-buttons">
-      <a href="<?php echo esc_url('https://www.facebook.com/groups/417324151803310/', 'eco'); ?>"><img src="<?php echo get_stylesheet_directory_uri();?>/img/f_button.png" width="45px" height="45px" alt="facebook" /></a>
-      <a href="<?php echo esc_url('https://twitter.com/ecostagepledge', 'eco' ); ?>"><img src="<?php echo get_stylesheet_directory_uri();?>/img/t_button.png" width="45px" height="45px" alt="twitter" /></a>
+      <a href="<?php echo esc_url('https://www.facebook.com/groups/417324151803310/', 'eco'); ?>" class="sl">f</a>
+      <a href="<?php echo esc_url('https://twitter.com/ecostagepledge', 'eco' ); ?>" class="sl">t</a>
     </div>
     <div id="footer-text-right">
       <p>Join us in building a community of ecologically minded theatre makers</p>
@@ -22,7 +22,7 @@
   </div>
 <?php is_front_page() ? get_sidebar('footer'): '' ; ?>
 		<div class="site-info">
-			Site: <a href="<?php echo esc_url( __( 'http://madeso.uk/r/eco/', 'eco' ) ); ?>">made<strong>so</strong></a>
+      Site: <a href="<?php echo esc_url( __( 'http://madeso.uk/', 'eco' ) ); ?>" rel="designer">made<strong>so</strong></a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
